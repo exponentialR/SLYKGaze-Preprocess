67bcf00761d7856350c7ab5177378412c1e46321
solua <soluadebayo@gmail.com>
2022-05-31 01:23:25 +0100

    corrected version without class commit

--- videostreams.py
+++ videostreams.py
@@ -1,54 +1,19 @@
+
 import cv2
-from pathlib import Path
-import numpy as np
-import os
 
+cap = cv2.VideoCapture(0)
+dim = (640, 480)
 
-class cropStream:
-    def __init__(self, source, cap=None, c_x=0, c_y=0, c_h=100, c_w=100):
-        # self.image = None
-        self.cap = cap
-        self.source = source
-        if self.cap is None and self.source == 0:
-            self.cap = cv2.VideoCapture(self.source)
-        else:
-            # self.cap is None and source != 0:
-            self.cap = cv2.VideoCapture(Path(source))
-        self.camera_open = self.cap.isOpened()
-        self.c_h = c_h
-        self.c_x = c_x
-        self.c_y = c_y
-        self.c_w = c_w
-        self.width = None
-        self.height = None
-        self.image = None
-        self.crop = None
-
-    # def get_shape(self):
-    #     if self.width is None:
-    #         self.height, self.width = self.image[:2]
-    #     return self.height, self.width
-
-    def get_cropped(self):
-        while self.camera_open:
-            if self.image and self.crop is None:
-                ret, self.image = self.cap.read()
-
-                if ret:
-                    self.crop = self.image[self.c_y:self.c_y + self.c_h, self.c_x:self.c_x + self.c_w]
-
-                    cv2.imshow('Original Frame', self.image)
-                    cv2.imshow('Cropped Frame', self.crop)
-                else:
-                    break
-        self.cap.release()
-        cv2.destroyAllWindows()
-
-
-def main():
-    source = 0
-    cropStream(source)
-
-
-if __name__ == '__main__':
-    main()
+while True:
+    ret, image = cap.read()
+    print(image.shape[:2])
+    crop = image[100:200 + 100, 0:100 + 100]
+    print(crop.shape)
+    resized_image = cv2.resize(crop, dim, interpolation=cv2.INTER_AREA)
+    print(resized_image.shape)
+    cv2.imshow('Original Window', image)
+    cv2.imshow('Cropped Window', resized_image)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
